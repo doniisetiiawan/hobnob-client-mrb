67a562a5f21caff31f32f09157851104f3d71789
Doni Setiawan <donisetiawan.050@gmail.com>
2019-10-08 09:17:08 +0700

    Add E2E test

---
+++ spec/cucumber/steps/interactions/element.js
@@ -0,0 +1,10 @@
+import { When } from 'cucumber';
+import { By } from 'selenium-webdriver';
+
+When(
+  /^user clicks on the ["']([.#\w-]+)["'] element$/,
+  async function (selector) {
+    const element = await this.driver.findElement(By.css(selector));
+    return element.click();
+  },
+);
